b4918eeaac488f1057f5887d0ef697cdd556e61c
Philipp Schader <philipp.schader@igd.fraunhofer.de>
2016-11-25 14:48:59 +0100

    LayoutPanel: Introduced layout object painter

--- ui/src/main/java/de/javagl/layoutanalyzer/ui/LayoutPanel.java
+++ ui/src/main/java/de/javagl/layoutanalyzer/ui/LayoutPanel.java
@@ -28,14 +28,12 @@ package de.javagl.layoutanalyzer.ui;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.util.List;
 import java.util.Objects;
 
@@ -44,210 +42,225 @@ import javax.swing.JPanel;
 import de.javagl.geom.ArrowCreator;
 import de.javagl.geom.Arrows;
 import de.javagl.geom.Points;
-import de.javagl.layoutanalyzer.Layout;
 import de.javagl.layoutanalyzer.AspectData;
+import de.javagl.layoutanalyzer.Layout;
 import de.javagl.layoutanalyzer.LayoutAspects;
 import de.javagl.layoutanalyzer.aspects.Aspect;
 import de.javagl.layoutanalyzer.objects.LayoutObject;
+import de.javagl.layoutanalyzer.painter.DefaultLayoutObjectPainter;
+import de.javagl.layoutanalyzer.painter.LayoutObjectPainter;
 import de.javagl.layoutanalyzer.utils.Colors;
 
 /**
- * A panel that shows a {@link Layout} consisting of {@link LayoutObject}s, and
- * optionally visualizes {@link AspectData}
+ * A panel that shows a {@link Layout} consisting of {@link LayoutObject}s, and optionally
+ * visualizes {@link AspectData}
  */
 public class LayoutPanel<T extends LayoutObject> extends JPanel {
-	/**
-	 * Serial UID
-	 */
-	private static final long serialVersionUID = 4678709294951136752L;
+  /**
+   * Serial UID
+   */
+  private static final long serialVersionUID = 4678709294951136752L;
 
-	/**
-	 * The default font that will be used for the labels of the
-	 * {@link LayoutObject}s
-	 */
-	private static final Font DEFAULT_FONT = new Font("Sans Serif", Font.PLAIN, 9);
+  /**
+   * The default font that will be used for the labels of the {@link LayoutObject}s
+   */
+  private static final Font DEFAULT_FONT = new Font("Sans Serif", Font.PLAIN, 9);
 
-	/**
-	 * The {@link Layout} that is shown in this panel
-	 * 
-	 */
-	private final Layout<T> layout;
+  /**
+   * The {@link Layout} that is shown in this panel
+   * 
+   */
+  private final Layout<T> layout;
 
-	/**
-	 * The optional {@link LayoutAspects} that may be shown in this panel
-	 */
-	private LayoutAspects layouterData;
+  /**
+   * The optional {@link LayoutAspects} that may be shown in this panel
+   */
+  private LayoutAspects layouterData;
 
-	/**
-	 * Whether the forces of any given {@link LayoutObject} are painted.
-	 */
-	private boolean showForces = true;
+  /**
+   * The {@link LayoutObjectPainter} used to draw the {@link LayoutObject}s in this panel. Defaults
+   * to {@link DefaultLayoutObjectPainter}
+   */
+  private LayoutObjectPainter<T> layoutObjectPainter;
 
-	/**
-	 * Whether the shapes of any given {@link LayoutObject} are painted
-	 */
-	private boolean showLayoutObjects = true;
+  /**
+   * Whether the forces of any given {@link LayoutObject} are painted.
+   */
+  private boolean showForces = true;
 
-	/**
-	 * Default constructor
-	 * 
-	 * @param layout
-	 *            The {@link Layout} that should be shown in this panel
-	 */
-	public LayoutPanel(Layout<T> layout) {
-		Objects.requireNonNull(layout, "The layout is null");
-		this.layout = layout;
-	}
+  /**
+   * Whether the shapes of any given {@link LayoutObject} are painted
+   */
+  private boolean showLayoutObjects = true;
 
-	/**
-	 * Set the {@link LayoutAspects} that should be shown in this panel. If this
-	 * is <code>null</code>, then no {@link LayoutAspects} will be shown.
-	 * 
-	 * @param layouterData
-	 *            The {@link LayoutAspects}
-	 */
-	public void setLayouterData(LayoutAspects layouterData) {
-		this.layouterData = layouterData;
-		repaint();
-	}
+  /**
+   * Default constructor
+   * 
+   * @param layout
+   *          The {@link Layout} that should be shown in this panel
+   */
+  public LayoutPanel(Layout<T> layout, LayoutObjectPainter<T> layoutObjectPainter) {
+    super();
+    Objects.requireNonNull(layout, "The layout is null");
+    this.layout = layout;
+    setLayoutObjectPainter(layoutObjectPainter);
+  }
+  
+  /**
+   * Default constructor
+   * 
+   * @param layout
+   *          The {@link Layout} that should be shown in this panel
+   */
+  public LayoutPanel(Layout<T> layout) {
+    this(layout, new DefaultLayoutObjectPainter<T>());
+  }
 
-	/**
-	 * Returns an unmodifiable view on the {@link LayoutObject}s
-	 * 
-	 * @return The {@link LayoutObject}s
-	 */
-	public List<T> getLayoutObjects() {
-		return layout.getLayoutObjects();
-	}
+  /**
+   * @param layoutObjectPainter
+   *          new {@link LayoutObjectPainter} used in subsequent {@link #paint(Graphics)}
+   */
+  public void setLayoutObjectPainter(LayoutObjectPainter<T> layoutObjectPainter) {
+    Objects.requireNonNull(layoutObjectPainter, "Painter is null");
+    this.layoutObjectPainter = layoutObjectPainter;
+  }
 
-	/**
-	 * Add the given {@link LayoutObject} to this layout
-	 * 
-	 * @param layoutObject
-	 *            The {@link LayoutObject}
-	 */
-	public void addLayoutObject(T layoutObject) {
-		layout.addLayoutObject(layoutObject);
-		repaint();
-	}
+  /**
+   * Set the {@link LayoutAspects} that should be shown in this panel. If this is <code>null</code>,
+   * then no {@link LayoutAspects} will be shown.
+   * 
+   * @param layouterData
+   *          The {@link LayoutAspects}
+   */
+  public void setLayouterData(LayoutAspects layouterData) {
+    this.layouterData = layouterData;
+    repaint();
+  }
 
-	/**
-	 * Remove the given {@link LayoutObject} from this layout
-	 * 
-	 * @param layoutObject
-	 *            The {@link LayoutObject}
-	 */
-	public boolean removeLayoutObject(T layoutObject) {
-		layout.removeLayoutObject(layoutObject);
-		repaint();
-		return true;
-	}
+  /**
+   * Returns an unmodifiable view on the {@link LayoutObject}s
+   * 
+   * @return The {@link LayoutObject}s
+   */
+  public List<T> getLayoutObjects() {
+    return layout.getLayoutObjects();
+  }
 
-	@Override
-	protected void paintComponent(Graphics gr) {
-		super.paintComponent(gr);
-		Graphics2D g = (Graphics2D) gr;
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+  /**
+   * Add the given {@link LayoutObject} to this layout
+   * 
+   * @param layoutObject
+   *          The {@link LayoutObject}
+   */
+  public void addLayoutObject(T layoutObject) {
+    layout.addLayoutObject(layoutObject);
+    repaint();
+  }
 
-		if (showLayoutObjects )
-			paintLayoutObjects(g);
-		if (showForces)
-			paintLayouterData(g);
-	}
+  /**
+   * Remove the given {@link LayoutObject} from this layout
+   * 
+   * @param layoutObject
+   *          The {@link LayoutObject}
+   */
+  public boolean removeLayoutObject(T layoutObject) {
+    layout.removeLayoutObject(layoutObject);
+    repaint();
+    return true;
+  }
 
-	/**
-	 * Paint the {@link LayoutObject}s of the {@link Layout} into the given
-	 * graphics.
-	 * 
-	 * @param g
-	 *            The graphics
-	 */
-	private void paintLayoutObjects(Graphics2D g) {
-		g.setFont(DEFAULT_FONT);
-		FontMetrics fontMetrics = g.getFontMetrics();
+  @Override
+  protected void paintComponent(Graphics gr) {
+    super.paintComponent(gr);
+    Graphics2D g = (Graphics2D) gr;
+    g.setColor(Color.WHITE);
+    g.fillRect(0, 0, getWidth(), getHeight());
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		List<T> layoutObjects = layout.getLayoutObjects();
-		for (LayoutObject layoutObject : layoutObjects) {
-			AffineTransform scale = AffineTransform.getScaleInstance(getWidth(), getHeight());
-			Point2D position = layoutObject.getPosition();
-			scale.transform(position, position);
+    if (showLayoutObjects)
+      paintLayoutObjects(g);
+    if (showForces)
+      paintForces(g);
+  }
 
-			Shape shape = layoutObject.getShape();
-			AffineTransform at = AffineTransform.getTranslateInstance(position.getX(), position.getY());
-			at.scale(getWidth(), getHeight());
-			Shape paintedShape = at.createTransformedShape(shape);
-			g.setColor(new Color(32, 32, 32, 32));
-			g.fill(paintedShape);
-			g.setColor(Color.GRAY);
-			g.draw(paintedShape);
+  /**
+   * Paint the {@link LayoutObject}s of the {@link Layout} into the given graphics.
+   * 
+   * @param g
+   *          The graphics
+   */
+  private void paintLayoutObjects(Graphics2D g) {
 
-			String label = layoutObject.getLabel();
-			Rectangle2D stringBounds = fontMetrics.getStringBounds(label, g);
-			int sx = (int) (position.getX() - stringBounds.getWidth() * 0.5);
-			int sy = (int) (position.getY() + stringBounds.getHeight() * 0.5);
-			g.drawString(label, sx, sy);
-		}
-	}
+    g.setFont(DEFAULT_FONT);
+    double scaleFactor = Math.min(getWidth(), getHeight());
+    final AffineTransform scale = AffineTransform.getScaleInstance(scaleFactor, scaleFactor);
+    final AffineTransform transform = g.getTransform();
+    List<T> layoutObjects = layout.getLayoutObjects();
+    for (T layoutObject : layoutObjects) {
+      Point2D position = layoutObject.getPosition();
+      scale.transform(position, position);
+      g.translate(position.getX(), position.getY());
+      layoutObjectPainter.paint(layoutObject, g);
+      g.setTransform(transform);
+    }
+  }
 
-	/**
-	 * Paint the current {@link LayoutAspects}, if it is not <code>null</code>,
-	 * into the given graphics
-	 * 
-	 * @param g
-	 *            The graphics
-	 */
-	private void paintLayouterData(Graphics2D g) {
-		if (layouterData == null) {
-			return;
-		}
-		ArrowCreator arrowCreator = Arrows.create().setAbsoluteHeadLength(15).setAbsoluteHeadWidth(10);
+  /**
+   * Paint the current {@link LayoutAspects}, if it is not <code>null</code>, into the given
+   * graphics
+   * 
+   * @param g
+   *          The graphics
+   */
+  private void paintForces(Graphics2D g) {
+    if (layouterData == null) {
+      return;
+    }
+    ArrowCreator arrowCreator = Arrows.create().setAbsoluteHeadLength(15).setAbsoluteHeadWidth(10);
 
-		List<T> layoutObjects = layout.getLayoutObjects();
-		for (LayoutObject layoutObject : layoutObjects) {
-			AffineTransform scale = AffineTransform.getScaleInstance(getWidth(), getHeight());
-			Point2D position = layoutObject.getPosition();
-			scale.transform(position, position);
-			
-			int n = 0;
-			for (Aspect aspect : layouterData.getAspects()) {
-				AspectData layoutData = layouterData.getLayoutData(aspect);
+    double scaleFactor = Math.min(getWidth(), getHeight());
+    final AffineTransform scale = AffineTransform.getScaleInstance(scaleFactor, scaleFactor);
 
-				g.setColor(Colors.getColorWithAlpha(n, 0.25));
+    List<T> layoutObjects = layout.getLayoutObjects();
+    for (LayoutObject layoutObject : layoutObjects) {
+      Point2D position = layoutObject.getPosition();
+      scale.transform(position, position);
+      int n = 0;
+      for (Aspect aspect : layouterData.getAspects()) {
+        AspectData layoutData = layouterData.getLayoutData(aspect);
 
-				Point2D force = layoutData.getForce(layoutObject);
-				scale.transform(force, force);
-				Point2D tip = Points.add(position, force, null);
-				Shape arrow = arrowCreator.buildShape(position, tip, 1.0);
-				g.fill(arrow);
+        g.setColor(Colors.getColorWithAlpha(n, 0.25));
 
-				g.setColor(Colors.getColor(n));
-				double aspectWeight = layoutData.getWeight();
-				Point2D tipScaled = Points.addScaled(position, aspectWeight, force, null);
-				Shape arrowScaled = arrowCreator.buildShape(position, tipScaled, 3.0);
-				g.fill(arrowScaled);
+        Point2D force = layoutData.getForce(layoutObject);
+        scale.transform(force, force);
+        Point2D tip = Points.add(position, force, null);
+        Shape arrow = arrowCreator.buildShape(position, tip, 1.0);
+        g.fill(arrow);
 
-				n++;
-			}
-		}
-	}
+        g.setColor(Colors.getColor(n));
+        double aspectWeight = layoutData.getWeight();
+        Point2D tipScaled = Points.addScaled(position, aspectWeight, force, null);
+        Shape arrowScaled = arrowCreator.buildShape(position, tipScaled, 3.0);
+        g.fill(arrowScaled);
 
-	public boolean isShowForces() {
-		return showForces;
-	}
+        n++;
+      }
+    }
+  }
 
-	public void setShowForces(boolean showForces) {
-		this.showForces = showForces;
-	}
-	
-	public void setShowLayoutObjects(boolean showLayoutObjects)
-	{
-		this.showLayoutObjects = showLayoutObjects;
-	}
-	
-	public boolean isShowLayoutObjects()
-	{
-		return showLayoutObjects;
-	}
+  public boolean isShowForces() {
+    return showForces;
+  }
+
+  public void setShowForces(boolean showForces) {
+    this.showForces = showForces;
+  }
+
+  public void setShowLayoutObjects(boolean showLayoutObjects) {
+    this.showLayoutObjects = showLayoutObjects;
+  }
+
+  public boolean isShowLayoutObjects() {
+    return showLayoutObjects;
+  }
 }
